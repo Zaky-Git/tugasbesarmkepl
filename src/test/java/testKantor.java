b65c0af196d37e74482978463ffe28039b113794
Law1911 <epensusanto@gmail.com>
2024-06-11 13:26:01 +0700

    menambahkan test case

--- src/test/java/testKantor.java
+++ src/test/java/testKantor.java
@@ -1,3 +1,13 @@
+
+import java.util.List;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import model.Kantor;
+import model.Pegawai;
+import static org.junit.Assert.assertNotEquals;
+import org.junit.Before;
+import org.junit.Test;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +18,55 @@
  * @author mzaky
  */
 public class testKantor {
+    private Kantor kantor;
+
+    @Before
+    public void setUp() {
+        kantor = new Kantor("Zaky Corporation");
+        kantor.tambahPegawai(new Pegawai("Angela", 33, 8000000));
+        kantor.tambahPegawai(new Pegawai("Kevin", 38, 5000000));
+        kantor.tambahPegawai(new Pegawai("Stanley", 51, 10000000));
+    }
+
+    @Test
+    public void testTambahPegawai() {
+        Pegawai pegawai = new Pegawai("Jim", 30, 7000000);
+        kantor.tambahPegawai(pegawai);
+        assertTrue(kantor.getDaftarPegawai().contains(pegawai));
+    }
+
+    @Test
+    public void testHapusPegawai() {
+        kantor.hapusPegawai("Angela");
+        List<Pegawai> pegawai = kantor.getDaftarPegawai();
+        for (Pegawai p : pegawai) {
+            assertNotEquals("Angela", p.getNama());
+        }
+    }
+
+    @Test
+    public void testUrutkanSesuaiGaji() {
+        kantor.urutkanSesuaiGaji();
+        List<Pegawai> pegawai = kantor.getDaftarPegawai();
+        assertEquals("Kevin", pegawai.get(0).getNama());
+        assertEquals("Angela", pegawai.get(1).getNama());
+        assertEquals("Stanley", pegawai.get(2).getNama());
+    }
+
+    @Test
+    public void testUrutkanSesuaiNama() {
+        kantor.urutkanSesuaiNama();
+        List<Pegawai> pegawai = kantor.getDaftarPegawai();
+        assertEquals("Angela", pegawai.get(0).getNama());
+        assertEquals("Kevin", pegawai.get(1).getNama());
+        assertEquals("Stanley", pegawai.get(2).getNama());
+    }
+
+    @Test
+    public void testGetSesuaiUmur() {
+        List<Pegawai> pegawai = kantor.getSesuaiUmur(38);
+        assertEquals(1, pegawai.size());
+        assertEquals("Kevin", pegawai.get(0).getNama());
+    }
     
 }
--- src/test/java/testPegawai.java
+++ src/test/java/testPegawai.java
@@ -1,3 +1,9 @@
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import model.Pegawai;
+import org.junit.Test;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +14,29 @@
  * @author mzaky
  */
 public class testPegawai {
+    @Test
+    public void testGetNama() {
+        Pegawai pegawai = new Pegawai("John Doe", 30, 5000000);
+        assertEquals("John Doe", pegawai.getNama());
+    }
+
+    @Test
+    public void testGetUmur() {
+        Pegawai pegawai = new Pegawai("John Doe", 30, 5000000);
+        assertEquals(30, pegawai.getUmur());
+    }
+
+    @Test
+    public void testGetGaji() {
+        Pegawai pegawai = new Pegawai("John Doe", 30, 5000000);
+        assertEquals(5000000, pegawai.getGaji(), 0.0);
+    }
+
+    @Test
+    public void testCompareTo() {
+        Pegawai pegawai1 = new Pegawai("John Doe", 30, 5000000);
+        Pegawai pegawai2 = new Pegawai("Jane Doe", 25, 6000000);
+        assertTrue(pegawai1.compareTo(pegawai2) < 0);
+    }
     
 }
