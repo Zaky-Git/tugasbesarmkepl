b0aa372aa32f775dccfd122644f009bcf60d568c
MUHAMMAD ZAKY FATHURAHIM <46282283+Zaky-Git@users.noreply.github.com>
2024-06-13 20:13:30 +0700

    Update komparatorNama.java

--- src/main/java/komparasi/komparatorNama.java
+++ src/main/java/komparasi/komparatorNama.java
@@ -12,6 +12,7 @@ import model.Pegawai;
 public class komparatorNama implements Comparator<Pegawai> {
     @Override
     public int compare(Pegawai o1, Pegawai o2){
+        System.out.println("a7x is the best");
         return  o1.getNama().compareTo(o2.getNama());
     }
 }
